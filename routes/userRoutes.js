47e2a26c0212f6474c73fd3d5f3495db7f1e8308
ORandoNat <nathanorando1@gmail.com>
2026-06-26 18:37:19 -0500

    Replace bcrypt with bcryptjs for Render compatibility

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 // userRoutes.js - Routes for user sign up and login
 
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
